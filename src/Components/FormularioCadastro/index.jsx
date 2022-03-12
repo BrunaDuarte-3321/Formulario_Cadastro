c6a3617b5fdbc2f1ae7bc8eb1cf997ca09cfdc11
mir4nd4s <62619506+BrunaDuarte-3321@users.noreply.github.com>
2022-03-12 09:01:57 -0300

    Usando Hook useState para manipular

--- src/Components/FormularioCadastro/index.jsx
+++ src/Components/FormularioCadastro/index.jsx
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
 
 const FormularioCadastro = () => {
-  return (
-    <>
-      <form action="">
-        <TextField
-          id="nome"
-          label="Nome"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          id="Sobrenome"
-          label="Sobrenome"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          id="cpf"
-          label="CPF"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <FormControlLabel
-          control={<Switch name="promocoes" defaultChecked />}
-          label="Promoções"
-        />
-        <FormControlLabel
-          control={<Switch name="novidade" defaultChecked />}
-          label="Novidades"
-        />
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
 
-        <Button variant="contained">Cadastrar</Button>
-      </form>
-    </>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(nome, sobrenome);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={nome}
+        onChange={({ target }) => {
+          let tmpNome = target.value;
+
+          if (tmpNome.length >= 3) {
+            tmpNome = tmpNome.substring(0, 3);
+          }
+          setNome(tmpNome);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        value={sobrenome}
+        onChange={({ target }) => setSobrenome(target.value)}
+        id="Sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <FormControlLabel
+        control={<Switch name="promocoes" defaultChecked />}
+        label="Promoções"
+      />
+      <FormControlLabel
+        control={<Switch name="novidade" defaultChecked />}
+        label="Novidades"
+      />
+
+      <Button variant="contained">Cadastrar</Button>
+    </form>
   );
 };
 
